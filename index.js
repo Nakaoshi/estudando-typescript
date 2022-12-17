104d9e7af3fce6e090ed47a9d660cfd3c9c8d814
Nakaoshi <89528377+Nakaoshi@users.noreply.github.com>
2022-12-16 21:22:42 -0300

    testando o compilador do typescript

---
+++ index.js
@@ -0,0 +1,7 @@
+var pessoa = "yes";
+console.log(pessoa);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ index.ts
@@ -0,0 +1,10 @@
+const pessoa = "yes";
+console.log(pessoa);
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+inputs.forEach(input =>{
+    console.log(input);
+})
